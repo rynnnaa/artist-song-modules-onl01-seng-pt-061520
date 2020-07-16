e0dcaee8de42401b450991834280f67a9ca55a69
Ryna Truong <rynatruong@outlook.com>
2020-07-16 17:53:03 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,18 +1,7 @@
-module Memorable
+module Findable
   module ClassMethods
-    def reset_all
-      self.all.clear
-    end
-  
-    def count
-      self.all.count
+    def find_by_name(name)
+      self.all.detect{|a| a.name == name}
     end
   end
-    
-  module InstanceMethods
-    def initialize
-      self.class.all << self
-    end
-  end
-
 end
